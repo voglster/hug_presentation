db8e233fda92a99a54a682c09312637237144a34
James Vogel <jim.m.vogel@gmail.com>
2018-12-28 14:27:15 -0600

    re-arranging for smoother talk

--- demo.py
+++ demo.py
@@ -2,16 +2,15 @@ import hug
 
 data = []
 
-@hug.get("/save", examples="word=test")
-def save(word):
-    data.append(word)
+@hug.get('/save', examples='text_to_save=testing123')
+def save(text_to_save):
+    data.append(text_to_save)
     return True
 
-@hug.get("/data")
+@hug.get('/data')
 def get_data():
     return data
 
-@hug.get("/clear")
-def clear_data():
+@hug.get('/clear')
+def get_data():
     data.clear()
-    return True
